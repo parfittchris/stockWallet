bfef5ae0c176acef3e8eadb729c91d05373d2c08
Chris Parfitt <parfitt.chris7@gmail.com>
2020-03-20 09:36:38 -0400

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: { format: :json} do
+    resources :users, only: [:create, :show, :destroy]
+    resources :stocks, only: [:create, :update, :destroy]
+    resources :transactions, only: [:create, :index]
+    resource :session, only: [:create, :destroy, :show]
+  end
+
+  get '*path' => redirect('/')
+
+end
+
